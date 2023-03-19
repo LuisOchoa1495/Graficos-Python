6487cd4674c5090ef9d4ac366ed8e76e5fd9da79
LENOVO <luisochoa.1495@gmail.com>
2023-03-19 00:57:54 -0500

    19/03/2023

---
+++ 4-Diagrama_dispersion_plt.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+tiempo_algebra = [1, 3, 5, 7]
+notas_algebra = [10, 12, 14, 16]
+tiempo_quimica = [2, 4 , 6 , 8]
+notas_quimica = [8, 10, 11.5, 13]
+
+#Configurar las características del gráfico
+plt.scatter(tiempo_algebra, notas_algebra, label = 'Algebra',color = 'red')
+plt.scatter(tiempo_quimica, notas_quimica,label = 'Quimica', color = 'purple')
+
+#Definir título y nombres de ejes
+plt.title('Gráfico de dispersión')
+plt.ylabel('Tiempo de estudio')
+plt.xlabel('Horas de estudio')
+
+#Mostrar leyenda y figura
+plt.legend()
+plt.grid()
+plt.show()
