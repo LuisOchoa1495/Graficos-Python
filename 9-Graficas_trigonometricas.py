7a6ab62821d7435016e8664fb0252b8c1f098fdf
LENOVO <luisochoa.1495@gmail.com>
2023-04-10 16:59:05 -0500

    10/04/2023

--- 9-Graficas_trigonometricas.py
+++ 9-Graficas_trigonometricas.py
@@ -10,10 +10,12 @@ x = np.linspace(-2*np.pi, 2*np.pi, 1000)
 points = [i * np.pi/2 for i in range(-4, 5)]
 labels = ["-2π", "-3π/2", "-π", "-π/2", "0", "π/2", "π", "3π/2", "2π"]
 
-fig, ax  = plt.subplots()
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+
 ax.plot(x, fn_1(x),label="Seno")
-ax.plot(x, fn_2(x),label="Coseno")
-ax.plot(x, fn_3(x),label="Tangente")
+#ax.plot(x, fn_2(x),label="Coseno")
+#ax.plot(x, fn_3(x),label="Tangente")
 ax.set_xticks(points)
 ax.set_xticklabels(labels)
 
