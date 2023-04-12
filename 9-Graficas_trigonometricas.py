60c9b5b47d929a8924bb5932830be3ebd0f462c8
LENOVO <luisochoa.1495@gmail.com>
2023-04-12 16:45:35 -0500

    12/04/2023

--- 9-Graficas_trigonometricas.py
+++ 9-Graficas_trigonometricas.py
@@ -1,4 +1,4 @@
-import math
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,7 +7,7 @@ fn_1 = np.sin
 fn_2 = np.cos
 fn_3 = np.tan
 
-x = np.linspace(-2*np.pi, 2*np.pi)
+x = np.linspace(-2*np.pi, 2*np.pi,1000)
 points = [i * np.pi/2 for i in range(-4, 5)]
 labels = ["-2π", "-3π/2", "-π", "-π/2", "0", "π/2", "π", "3π/2", "2π"]
 
@@ -20,9 +20,9 @@ ax.plot(x, fn_3(x),label="Tangente")
 ax.set_xticks(points)
 ax.set_xticklabels(labels)
 
-plt.ylim(-1, 1)
-ax.hlines(0, -7,7, "k")
-ax.vlines(0, -5, 5, "k")
+plt.ylim(-5, 5)
+ax.hlines(0, -7,7, "black")
+ax.vlines(0, -5, 5, "black")
 plt.title("Graficas Trigonometricas")
 plt.grid()
 plt.legend()
--- TRIGONOMETRICAS.py
+++ TRIGONOMETRICAS.py
@@ -1,28 +1,28 @@
-import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-#funcioens
-fn_1 = np.sin
-fn_2 = np.cos
-fn_3 = np.tan
+#funciones
+fn_1=np.sin
+fn_2=np.cos
+fn_3=np.tan
 
-x=np.linspace(-2*np.pi, 2*np.pi)
-points = [i * np.pi/2 for i in range(-4, 5)]
-labels = ["-2π", "-3π/2", "-π", "-π/2", "0", "π/2", "π", "3π/2", "2π"]
+x=np.linspace(-2*np.pi, 2*np.pi,1000)
+points=[i* np.pi/2 for i in range(-4,5)]
+labels=["-2π", "-3π/2", "-π", "-π/2", "0", "π/2", "π", "3π/2", "2π"]
 
 fig=plt.figure()
 ax=fig.add_subplot(1,1,1)
 
-ax.plot(x, fn_1(x),label="Seno")
-ax.plot(x, fn_2(x),label="Coseno")
-ax.plot(x, fn_3(x),label="Tangente")
+ax.plot(x,fn_1(x),label="Seno")
+ax.plot(x,fn_2(x),label="Coseno")
+ax.plot(x,fn_3(x),label="Tangente")
+
 ax.set_xticks(points)
 ax.set_xticklabels(labels)
+plt.ylim(-5,5)
 
-plt.ylim(-1, 1)
-ax.hlines(0, -7,7, "k")
-ax.vlines(0, -5, 5, "k")
+ax.hlines(0,-7,7,"black")
+ax.vlines(0,-5,5, "black")
 plt.title("Graficas Trigonometricas")
 plt.grid()
 plt.legend()
