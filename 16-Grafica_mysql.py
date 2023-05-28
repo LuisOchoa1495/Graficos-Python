8fcc11807e734ba469f33da604a749dfc12cc0e4
LENOVO <luisochoa.1495@gmail.com>
2023-05-27 22:51:53 -0500

    27/05/2022

---
+++ 16-Grafica_mysql.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+conexion = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      passwd="",
+      database="prueba"
+    )
+####### end of connection ####
+my_data1 = pd.read_sql("SELECT * FROM productos",conexion)
+my_data2 = pd.read_sql("SELECT c.categoria,sum(p.cantidad) as total FROM productos as p INNER JOIN categorias as c ON c.idcategoria=p.categoria GROUP BY c.categoria",conexion)
+labels=my_data2['categoria']
+print(labels)
+my_data2.plot(y="total", kind="pie", autopct='%1.01f%%',labels=labels,legend=False)
+plt.title("Total de productos por categorias")
+plt.show()
+
+
+"""
+#CONSULTAS 
+SELECT * FROM productos;
+#GRAFICO
+SELECT c.categoria,sum(p.cantidad) FROM productos as p INNER JOIN categorias as c ON c.idcategoria=p.categoria GROUP BY c.categoria
+"""
