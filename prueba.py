d239759eca84dc43c7d9cb8e73d6880f4f7a7bbc
LENOVO <luisochoa.1495@gmail.com>
2023-06-15 16:59:57 -0500

    15/06/2023

--- prueba.py
+++ prueba.py
@@ -1,12 +1,24 @@
-import pandas as pd
+import pandas as pd 
 import matplotlib.pyplot as plt
 
-tables=pd.read_html('https://www.nbamaniacs.com/palmares-nba/')
-print(f'Número total de tablas: {len(tables)}')
+csv=pd.read_csv('fallecidos_covid.csv',sep=';')
+dataframe=pd.DataFrame(csv[{"FECHA_FALLECIMIENTO","SEXO","DEPARTAMENTO"}])
+sexo=dataframe["SEXO"].value_counts()
+departamento=dataframe["DEPARTAMENTO"].value_counts().head()
+print(departamento)
 
-datos=(tables[0].head())
-dataframe=pd.DataFrame(datos[{'Franquicia','Títulos'}])
-print(dataframe)
-dataframe.plot(x="Franquicia",kind="bar",rot=10)
-plt.title("Top 5 - Ganadores NBA")
+#Grafica 1
+plt.subplot(1,2,1)
+sexo.plot(kind="pie",autopct='%1.01f%%')
+plt.title("SEXO")
+
+#Grafica 2
+plt.subplot(1,2,2)
+departamento.plot(kind="bar",rot=20)
+plt.xticks(fontsize=9)
+plt.yticks(fontsize=9)
+plt.title("DEPARTAMENTO")
+
+
+plt.suptitle("GRAFICA COVID 19 - FALLECIDOS")
 plt.show()
