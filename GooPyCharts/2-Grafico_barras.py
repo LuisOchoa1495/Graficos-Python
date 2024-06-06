addcf8791fee265b77d4683032ccc0d8df88665c
LENOVO <luisochoa.1495@gmail.com>
2024-06-06 14:54:11 -0500

    06/06/2024

--- GooPyCharts/2-Grafico_barras.py
+++ GooPyCharts/2-Grafico_barras.py
@@ -1,7 +1,8 @@
 from gpcharts import figure
-grafico = figure('Competiciones UEFA (Clubes)')
+
+grafico=figure('Competiciones UEFA (CLUBES)')
 grafico.ylabel='Equipos de futbol'
-valoresx=['Cantidad de Titulos','Real Madrid','AC Milan','Liverpool','Barcelona','Bayern Munich']
+valoresx=['Cantidad de titulos','Real Madrid','AC Milan','Liverpool','Barcelona','Bayern Munich']
 valoresy=[['Champions League','Europa League'],
-       [15,2],[7,0],[6,3],[5,0],[6,1]]
-grafico.bar(valoresx,valoresy)
+          [15,2],[7,0],[6,3],[5,0],[6,1]]
+grafico.bar(valoresx, valoresy)
--- GooPyCharts/3-Grafico_dispersion.py
+++ GooPyCharts/3-Grafico_dispersion.py
@@ -1,7 +1,8 @@
-from gpcharts import figure
-grafico = figure('Evaluaciones y tendencia de calificaciones')
-grafico.ylabel="Calificaciones"
-grafico.xlabel="Pruebas realizadas"
-grafico.scatter([20,15,18,14,16,12,9,8,13,11,17], trendline=True)
+from gpcharts import figure 
+grafico=figure('Evaluaciones y tendencias de calificaciones')
+grafico.ylabel='Calificaciones'
+grafico.xlabel='Pruebas realizadas'
 
-#linea de tendencia permite analizar el comprotamiento de las calificaciones y asi predecir valores.
+grafico.scatter([20,15,18,14,16,12,9,8,13,11,17,20,20,20],trendline=True)
+
+#linea de tendencia permite analizar el comportamiento de las califiaciones y asi predecir o estimar.
